011b8ea755f5db0a06ab61123853c041480178df
Xiaolan Jiang <federerjiang@gmail.com>
2018-03-16 00:38:51 +0900

    update

--- predictor/data_loader.py
+++ predictor/data_loader.py
@@ -27,7 +27,7 @@ class TrainDataLoader:
         print(len(self.all_vp_unit))
         # pick a random viewport trace file
         # self.vp_idx = np.random.randint(low=0, high=len(self.all_vp_unit))
-        self.vp_idx = np.random.randint(low=0, high=47)
+        self.vp_idx = np.random.randint(low=0, high=45)
         self.vp_unit = self.all_vp_unit[self.vp_idx]
         self.unit_start_max = len(self.vp_unit) - LABEL_SAMPLE_LENGTH - 2
         self.unit_idx = 0
@@ -50,6 +50,7 @@ class TrainDataLoader:
                             vp_unit.append(self._str_to_float(parse[2:6]))
                 all_vp_time.append(vp_time)
                 all_vp_unit.append(vp_unit)
+                print(len(all_vp_unit))
 
         return all_vp_unit, all_vp_time
 
