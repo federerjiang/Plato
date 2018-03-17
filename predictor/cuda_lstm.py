e1bcd5ea7cf0a71cf9b0784ba389dbb4b6274bb7
Xiaolan Jiang <federerjiang@gmail.com>
2018-03-17 20:35:01 +0900

    update

--- predictor/cuda_lstm.py
+++ predictor/cuda_lstm.py
@@ -67,13 +67,13 @@ def train_model(model, learning_rate, data_loader, epoch=10, count_max=10000):
         model = model.cuda()
 
     loss_function = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     for poch in range(epoch):
         count = 0
         loss_avg = 0.0
 
         # optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9, nesterov=True)
-        optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-        learning_rate *= 0.2
+        # learning_rate *= 0.2
         for inputs, label in data_loader:
             # inputs = train_data[i: i+30]
             if count == count_max:
