b182cdb9412283610e91a7170382698f5c8b1338
Xiaolan Jiang <federerjiang@gmail.com>
2018-03-20 19:37:28 +0900

    update

--- predictor_cooked_dataset/validate_acc.py
+++ predictor_cooked_dataset/validate_acc.py
@@ -95,39 +95,41 @@ def other_predict(model, inputs, length=TEST_LABEL_LENGTH):
     return outputs
 
 
+def get_rotations(predicts, label):
+    predict_rolls = []
+    predict_pitchs = []
+    predict_yaws = []
+    label_rolls = []
+    label_pitchs = []
+    label_yaws = []
+
+    for sample in predicts:
+        predict_rolls.append(sample[0])
+        predict_pitchs.append(sample[1])
+        predict_yaws.append(sample[2])
+    for sample in label:
+        label_rolls.append(sample[0])
+        label_pitchs.append((sample[1]))
+        label_yaws.append(sample[2])
+    return predict_rolls, predict_pitchs, predict_yaws, label_rolls, label_pitchs, label_yaws
+
+
+def get_loss(loss_function, predicts, label):
+    predicts = torch.FloatTensor(predicts)
+    predicts = Variable(predicts, volatile=True)
+    label = torch.FloatTensor(label)
+    label = Variable(label, volatile=True)
+    loss = loss_function(predicts, label)
+    loss = math.sqrt(loss) * 180
+    return loss
+
+
 def validate_lstm_rotation_acc(test_data_loader, model_path, num_layers, hidden_size):
     def init_hidden(num_layers, hidden_size):
         hx = torch.nn.init.xavier_normal(torch.randn(num_layers, 1, hidden_size))
         cx = torch.nn.init.xavier_normal(torch.randn(num_layers, 1, hidden_size))
         hidden = (Variable(hx, volatile=True), Variable(cx, volatile=True))  # convert to Variable as late as possible
         return hidden
-
-    def get_rotations(predicts, label):
-        predict_rolls = []
-        predict_pitchs = []
-        predict_yaws = []
-        label_rolls = []
-        label_pitchs = []
-        label_yaws = []
-
-        for sample in predicts:
-            predict_rolls.append(sample[0])
-            predict_pitchs.append(sample[1])
-            predict_yaws.append(sample[2])
-        for sample in label:
-            label_rolls.append(sample[0])
-            label_pitchs.append((sample[1]))
-            label_yaws.append(sample[2])
-        return predict_rolls, predict_pitchs, predict_yaws, label_rolls, label_pitchs, label_yaws
-
-    def get_loss(loss_function, predicts, label):
-        predicts = torch.FloatTensor(predicts)
-        predicts = Variable(predicts, volatile=True)
-        label = torch.FloatTensor(label)
-        label = Variable(label, volatile=True)
-        loss = loss_function(predicts, label)
-        loss = math.sqrt(loss) * 180
-        return loss
 
     model = torch.load(model_path, map_location='cpu')
     model.hidden = init_hidden(num_layers, hidden_size)
@@ -165,34 +167,40 @@ def validate_lstm_tile_acc(test_data_loader, model_path, num_layers, hidden_size
 
 
 def validate_other_rotation_acc(model, test_data_loader):
+
+    loss_function = torch.nn.MSELoss()
+    loss_sum = 0.0
     count = 0
     count_acc = 0
-    for inputs, label in test_data_loader:
-        predicts = other_predict(model, inputs)
-        predicts = torch.FloatTensor(predicts)
-        predicts = Variable(predicts, volatile=True)
-        labels = torch.FloatTensor(label)
-        labels = Variable(labels, volatile=True)
-        loss = loss_function(predicts, labels)
-        loss = math.sqrt(loss) * 180
-        print(loss)
-        if loss <= 10:
-            count_acc += 1
-        count += 1
-        loss_sum += loss
-        if count == 10000:
-            break
-    print(count_acc)
-    print(count_acc / count)
-    print(loss_sum / count)
+    with open(str(TEST_LABEL_LENGTH) + 'lr-loss.txt', 'w') as f:
+        for inputs, label in test_data_loader:
+            predicts = other_predict(model, inputs)
+            predict_rolls, predict_pitchs, predict_yaws, label_rolls, label_pitchs, label_yaws = get_rotations(predicts,
+                                                                                                               label)
+            roll_loss = get_loss(loss_function, predict_rolls, label_rolls)
+            pitch_loss = get_loss(loss_function, predict_pitchs, label_pitchs)
+            yaw_loss = get_loss(loss_function, predict_yaws, label_yaws)
+
+            f.write(str(roll_loss) + ' ' + str(pitch_loss) + ' ' + str(yaw_loss) + '\n')
+            print(roll_loss, pitch_loss, yaw_loss)
+        # print(loss)
+        # if loss <= 10:
+        #     count_acc += 1
+            count += 1
+            # loss_sum += loss
+            if count == 100000:
+                break
+    # print(count_acc)
+    # print(count_acc / count)
+    # print(loss_sum / count)
     return loss_sum / count
 
 
 if __name__ == "__main__":
     test_data_loader = TestDataLoader()
-    validate_lstm_rotation_acc(test_data_loader, 'adam-lstm-128-1.model', 1, 128)
+    # validate_lstm_rotation_acc(test_data_loader, 'adam-lstm-128-1.model', 1, 128)
     # validate_other_rotation_acc(average, test_data_loader)
-    # validate_other_rotation_acc(lr, test_data_loader)
+    validate_other_rotation_acc(lr, test_data_loader)
 
 
 
