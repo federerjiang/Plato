db48483cb089e02cba9a6ebae91ccd640f7da541
Xiaolan Jiang <federerjiang@gmail.com>
2018-03-21 03:22:14 +0900

    update

--- predictor_new_cooked_dataset/mp_validate_acc.py
+++ predictor_new_cooked_dataset/mp_validate_acc.py
@@ -180,10 +180,10 @@ if __name__ == "__main__":
     # validate_other_rotation_acc(args, lr, test_data_loader)
     for length in [30, 60, 90]:
         args = Args(length)
-        main_validate_other(args, lr)
+        main_validate_other(args, lr, 'lr')
     for length in [30, 60, 90]:
         args = Args(length)
-        main_validate_other(args, average)
+        main_validate_other(args, average, 'average')
     for length in [30, 60, 90]:
         args = Args(length)
         main_validate_lstm(args, model_path, hidden_size, num_layers)
