5f68d446d10a3024835d79f12e1d1a4eb23f8a85
Xiaolan Jiang <federerjiang@gmail.com>
2018-03-21 11:06:50 +0900

    update

---
+++ predictor_new_cooked_dataset/analyze_loss.py
@@ -0,0 +1,65 @@
+
+
+def get_average_rotation(file_path):
+    roll = 0
+    pitch = 0
+    yaw = 0
+    count = 0
+    with open(file_path) as f:
+        for line in f:
+            parse = line.split()
+            roll += float(parse[0])
+            pitch += float(parse[1])
+            yaw += float(parse[2])
+            count += 1
+    return roll/count, pitch/count, yaw/count
+
+
+def get_thre_count(file_path, max):
+    roll = 0
+    pitch = 0
+    yaw = 0
+    with open(file_path) as f:
+        for line in f:
+            parse = line.split()
+            if float(parse[0]) < max:
+                roll += 1
+            if float(parse[1]) < max:
+                pitch += 1
+            if float(parse[2]) < max:
+                yaw += 1
+    return roll, pitch, yaw
+
+
+if __name__ == "__main__":
+    # result = get_average_rotation('30average-loss.txt')
+    # print(result)
+    # result = get_average_rotation('30lr-loss.txt')
+    # print(result)
+    # result = get_average_rotation('30lstm-128-1-loss.txt')
+    # print(result)
+    # result = get_average_rotation('60average-loss.txt')
+    # print(result)
+    # result = get_average_rotation('60lr-loss.txt')
+    # print(result)
+    # result = get_average_rotation('60lstm-128-1-loss.txt')
+    # print(result)
+
+    # result = get_thre_count('30average-loss.txt', 20)
+    # print(result)
+    # result = get_thre_count('30lr-loss.txt', 20)
+    # print(result)
+    # result = get_thre_count('30lstm-128-1-loss.txt', 20)
+    # print(result)
+    result = get_thre_count('60average-loss.txt', 20)
+    print(result)
+    result = get_thre_count('60lr-loss.txt', 20)
+    print(result)
+    result = get_thre_count('60lstm-128-1-loss.txt', 20)
+    print(result)
+    # result = get_thre_count('90average-loss.txt', 60)
+    # print(result)
+    # result = get_thre_count('90lr-loss.txt', 60)
+    # print(result)
+    # result = get_thre_count('90lstm-128-1-loss.txt', 20)
+    # print(result)
