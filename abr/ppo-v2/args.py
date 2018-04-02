1f9842c950cb6bc2ac89b1358f23e4ca339f1665
Xiaolan Jiang <federerjiang@gmail.com>
2018-04-02 13:10:57 +0900

    'update'

--- abr/ppo-v2/args.py
+++ abr/ppo-v2/args.py
@@ -37,8 +37,8 @@ class Args:
         self.video_bitrate = [180, 150, 120, 90, 60]  # for each tile, in Kbp/s
 
         # model training parameters
-        self.batch_size = 16
-        self.num_mini_batch = 4
+        self.batch_size = 1280
+        self.num_mini_batch = 32
         self.a_lr = 3e-4
         self.c_lr = 1e-4
         self.gamma = 0.99
@@ -47,9 +47,7 @@ class Args:
         self.ent_coef = 0.
         self.num_epoch = 10
         self.num_steps = 100
-        self.num_processes = 1
-        self.update_steps = 20
-        self.update_threshold = self.num_processes - 1
+        self.num_processes = 10
         self.max_episode_length = 100
         self.seed = 30
 
