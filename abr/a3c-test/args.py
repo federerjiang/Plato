2293ad9938d054fda81f6d0ecf7352121fe40c4b
Xiaolan Jiang <federerjiang@gmail.com>
2018-04-05 18:11:34 +0900

    'update'

--- abr/a3c-test/args.py
+++ abr/a3c-test/args.py
@@ -1,6 +1,6 @@
 class Args:
     def __init__(self):
-        self.env = 'PongDeterministic-v3'
+        self.env = 'PongDeterministic-v4'
         self.lr = 0.0001
         self.gamma = 0.99
         self.tau = 1.00
@@ -8,7 +8,7 @@ class Args:
         self.value_loss_coef = 0.5
         self.max_grad_norm = 50
         self.seed = 1
-        self.num_processes = 24
+        self.num_processes = 4
         self.num_steps = 20000
         self.max_episode_length = 1000000
         self.no_shared = False
