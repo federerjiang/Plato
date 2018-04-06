58dc7a59589c3ac8dc2cec8e4a0f015ede03ac0a
Xiaolan Jiang <federerjiang@gmail.com>
2018-04-06 11:38:25 +0900

    'update'

--- abr/a3c-test/train.py
+++ abr/a3c-test/train.py
@@ -10,8 +10,8 @@ from model import ActorCritic
 def ensure_shared_grads(model, shared_model):
     for param, shared_param in zip(model.parameters(),
                                    shared_model.parameters()):
-        if shared_param.grad is not None:
-            return
+        # if shared_param.grad is not None:
+        #     return
         shared_param._grad = param.grad
 
 
