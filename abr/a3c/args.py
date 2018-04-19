a297a57139985e1099285daf5084f4718efba08b
Xiaolan Jiang <federerjiang@gmail.com>
2018-04-19 17:43:57 +0900

    'update'

--- abr/a3c/args.py
+++ abr/a3c/args.py
@@ -52,7 +52,7 @@ class Args:
         self.max_grad_norm = 1
         self.seed = 1
         self.num_processes = 16
-        self.num_steps = 300
+        self.num_steps = 10000
         self.max_episode_length = 25
         self.seed = 30
 
