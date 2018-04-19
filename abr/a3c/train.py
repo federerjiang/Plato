e18a5a0189f68506a74171fcfbd264ae68f91748
Xiaolan Jiang <federerjiang@gmail.com>
2018-04-19 18:49:55 +0900

    'update'

--- abr/a3c/train.py
+++ abr/a3c/train.py
@@ -86,30 +86,30 @@ def train(rank, args, share_model, counter, lock,
                 # reward_sum = 0
                 break
 
-            R = torch.zeros(1, 1)
-            if not done:
-                logit, value = model(state.view(-1, 11, 8))
-                R = value.data
-            values.append(Variable(R))
-            policy_loss = 0
-            value_loss = 0
-            R = Variable(R)
-            gae = torch.zeros(1, 1)
-            for i in reversed(range(len(rewards))):
-                R = args.gamma * R + rewards[i]
-                advantage = R - values[i]
-                value_loss = value_loss + 0.5 * advantage.pow(2)
+        R = torch.zeros(1, 1)
+        if not done:
+            logit, value = model(state.view(-1, 11, 8))
+            R = value.data
+        values.append(Variable(R))
+        policy_loss = 0
+        value_loss = 0
+        R = Variable(R)
+        gae = torch.zeros(1, 1)
+        for i in reversed(range(len(rewards))):
+            R = args.gamma * R + rewards[i]
+            advantage = R - values[i]
+            value_loss = value_loss + 0.5 * advantage.pow(2)
 
-                delta_t = rewards[i] + args.gamma * values[i + 1].data - values[i].data
-                gae = gae * args.gamma * args.tau + delta_t
+            delta_t = rewards[i] + args.gamma * values[i + 1].data - values[i].data
+            gae = gae * args.gamma * args.tau + delta_t
 
-                policy_loss = policy_loss - log_probs[i] * Variable(gae) - entropy_coef * entropies[i]
+            policy_loss = policy_loss - log_probs[i] * Variable(gae) - entropy_coef * entropies[i]
 
-            optimizer.zero_grad()
-            (policy_loss + args.value_loss_coef * value_loss).backward()
-            torch.nn.utils.clip_grad_norm(model.parameters(), args.max_grad_norm)
-            ensure_shared_grads(model, share_model)
-            optimizer.step()
+        optimizer.zero_grad()
+        (policy_loss + args.value_loss_coef * value_loss).backward()
+        torch.nn.utils.clip_grad_norm(model.parameters(), args.max_grad_norm)
+        ensure_shared_grads(model, share_model)
+        optimizer.step()
 
 
             # print('update', rank)
